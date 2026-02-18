582abd24b445bd1d83453acea6a5a1c24428dad3
DrSuriya <155653384+DrSuriya@users.noreply.github.com>
2026-02-18 16:36:49 -0500

    Refactor server to handle custom message structure

--- IPC/server/src/server.cpp
+++ IPC/server/src/server.cpp
@@ -3,12 +3,13 @@
 #include <sys/neutrino.h>
 #include <sys/iofunc.h>
 #include <sys/dispatch.h>
+#include "shared_header.hpp"
 
 #define ATTACH_POINT "my_qnx_server"
 
 int main() {
-    struct _pulse pulse;
-
+    my_msg_t msg_con;
+	my_reply_t reply;
     name_attach_t *attach;
 
     if ((attach = name_attach(NULL, ATTACH_POINT, 0)) == NULL) { // dispatch, path, flags
@@ -19,10 +20,19 @@ int main() {
     printf("Waiting for client heartbeats...\n");
 
     while (1) {
-        int rcvid = MsgReceive(attach->chid, &pulse, sizeof(pulse), NULL); // chid, msg, msg size, msg info
+        int rcvid = MsgReceive(attach->chid, &msg_con, sizeof(msg_con), NULL);
+
+        if (rcvid == -1) break;
 
         if (rcvid == 0) {
-            printf("Heartbeat received (Code: %d, Value: %d)\n", pulse.code, pulse.value.sival_int);
+            printf("Pulse received (Code: %d, Value: %d)\n", msg_con.pulse.code, msg_con.pulse.value.sival_int);
+        } else {
+            printf("Message received (Text: '%s', Value: %d)\n", msg_con.msg.text, msg_con.msg.value);
+
+            reply.status = true;
+            snprintf(reply.reply_text, 64, "Acknowledged: %d", msg_con.msg.value);
+
+            MsgReply(rcvid, 0, &reply, sizeof(reply));
         }
     }
     name_detach(attach, 0);
